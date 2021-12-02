e6d564f942fbea588af4c25fa1cbb56d55669d7a
MarkB20 <91541086+MarkB20@users.noreply.github.com>
2021-12-02 00:00:59 +0000

    update: background

--- SFML/start.h
+++ SFML/start.h
@@ -11,12 +11,15 @@ class start
 private:
 	sf::Texture startButton;
 	sf::Sprite startButtonImage;
+	sf::Texture background;
+	sf::Sprite backgroundImage;
 
 
 public:
 
 	bool ReactionStart = false;
 	bool starting = true;
+	void createbackground(std::string _fileName);
 	void createStartButton1(std::string _fileName);
 	void CheckStart(std::string target, std::string faketarget, std::string background);
 	void drawStart(sf::RenderWindow& _win);
